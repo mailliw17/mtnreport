564adfc3eeaa502736c85b67123046c9550f1b0a
William <williamwirahardi@gmail.com>
2021-08-31 11:18:01 +0700

    new sql file

--- mtnreport - 31 agustus.sql
+++ mtnreport - 31 agustus.sql
@@ -12,27 +12,6 @@ CREATE TABLE `area` (
   PRIMARY KEY (`id_area`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `area` (`id_area`, `area`) VALUES
-(1,	'Intake Silo'),
-(2,	'Intake Tilting'),
-(3,	'Dryer 1'),
-(5,	'Dryer 2'),
-(6,	'Intake FM'),
-(7,	'Hammermill 1'),
-(8,	'Hammermill 2'),
-(9,	'Hammermill 3'),
-(10,	'Rollermill'),
-(11,	'Mixer A'),
-(12,	'Mixer B'),
-(13,	'Pelletmill 1'),
-(14,	'Pelletmill 2'),
-(15,	'Pelletmill 3'),
-(16,	'Pelletmill 4'),
-(17,	'Pelletmill 5'),
-(18,	'Pelletmill 6'),
-(19,	'Pelletmill 7'),
-(20,	'Pelletmill 8'),
-(21,	'kebun');
 
 DROP TABLE IF EXISTS `detail_work_order`;
 CREATE TABLE `detail_work_order` (
@@ -43,7 +22,7 @@ CREATE TABLE `detail_work_order` (
   `approved_work` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id_detail_work_order`),
   KEY `id_work_order` (`id_work_order`),
-  CONSTRAINT `detail_work_order_ibfk_3` FOREIGN KEY (`id_work_order`) REFERENCES `work_order` (`id_work_order`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `detail_work_order_ibfk_4` FOREIGN KEY (`id_work_order`) REFERENCES `work_order` (`id_work_order`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
@@ -98,18 +77,6 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `users` (`id`, `nama`, `username`, `password`, `role`, `grup`, `is_lapor`, `updated_at`) VALUES
-(1,	'William',	'wil',	'202cb962ac59075b964b07152d234b70',	'Admin',	'',	0,	NULL),
-(3,	'Admin',	'admin',	'21232f297a57a5a743894a0e4a801fc3',	'Admin',	'',	0,	NULL),
-(6,	'johan',	'jhn',	'202cb962ac59075b964b07152d234b70',	'Teknisi',	'Electrical',	1,	'2021-08-13'),
-(7,	'thomas',	'thomthom',	'202cb962ac59075b964b07152d234b70',	'Teknisi',	'Electrical',	1,	'2021-08-13'),
-(9,	'james bond',	'james1223',	'202cb962ac59075b964b07152d234b70',	'Teknisi',	'Automation',	1,	'2021-08-13'),
-(10,	'Teknisi saja',	'tektek',	'202cb962ac59075b964b07152d234b70',	'Teknisi',	'Electrical',	1,	'2021-08-13'),
-(38,	'Nanang Haruni',	'nanang',	'202cb962ac59075b964b07152d234b70',	'Supervisor-NonMTN',	'Warehouse',	0,	NULL),
-(40,	'Non enim dolor nostr',	'nugysyn',	'202cb962ac59075b964b07152d234b70',	'Karyawan-NonMTN',	'Warehouse',	0,	NULL),
-(42,	'Cipta Hartanto',	'cipta',	'202cb962ac59075b964b07152d234b70',	'Supervisor-NonMTN',	'Produksi',	0,	NULL),
-(43,	'Ducimus voluptatem',	'kelir',	'202cb962ac59075b964b07152d234b70',	'Karyawan-NonMTN',	'Produksi',	0,	NULL),
-(45,	'Dadang',	'dadang',	'202cb962ac59075b964b07152d234b70',	'Karyawan-NonMTN',	'Produksi',	0,	NULL);
 
 DROP TABLE IF EXISTS `work_order`;
 CREATE TABLE `work_order` (
@@ -118,6 +85,7 @@ CREATE TABLE `work_order` (
   `grup` varchar(50) NOT NULL,
   `area` varchar(50) NOT NULL,
   `permasalahan` text NOT NULL,
+  `bagian_teknisi` varchar(50) NOT NULL,
   `photo` varchar(50) NOT NULL,
   `request_by` varchar(50) NOT NULL,
   `approved_by` varchar(50) DEFAULT NULL,
@@ -128,4 +96,15 @@ CREATE TABLE `work_order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
--- 2021-08-13 03:49:12
+DROP TABLE IF EXISTS `work_order_ditolak`;
+CREATE TABLE `work_order_ditolak` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_work_order` varchar(50) NOT NULL,
+  `alasan` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_work_order` (`id_work_order`),
+  CONSTRAINT `work_order_ditolak_ibfk_1` FOREIGN KEY (`id_work_order`) REFERENCES `work_order` (`id_work_order`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2021-08-31 04:17:15
